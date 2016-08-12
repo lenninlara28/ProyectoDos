e5a1160b5f8a1bf6f67ca6d0d4e8c06c6322f64f
“Lenninlara” <lenninlara28@hotmail.com>
2016-08-12 11:07:29 -0500

    Proyecto creado

---
+++ src/proyectodos/ProyectoDos.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package proyectodos;
+
+/**
+ *
+ * @author llara11
+ */
+public class ProyectoDos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
